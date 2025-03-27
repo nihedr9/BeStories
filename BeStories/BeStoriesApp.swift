2d7dba83927ab43dabe9dde800803bda90e55155
Nihed Majdoub <majdoub.nihed@gmail.com>
2025-03-27 10:15:57 +0100

    Initial Commit

---
+++ BeStories/BeStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeStoriesApp.swift
+//  BeStories
+//
+//  Created by Nihed Majdoub on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BeStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeStoriesTests/BeStoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeStoriesTests.swift
+//  BeStoriesTests
+//
+//  Created by Nihed Majdoub on 27/03/2025.
+//
+
+import Testing
+@testable import BeStories
+
+struct BeStoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
